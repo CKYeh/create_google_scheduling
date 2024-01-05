1b7810b47c902f6de6170e716582025764668d16
ck.yeh <ck.yeh@axiomtek.com.tw>
2024-01-05 08:10:55 +0800

    create google calendar, not yet

--- lunarToDate.py
+++ lunarToDate.py
@@ -1,7 +1,17 @@
 from zhdate import ZhDate
+# zhdate is limited to before 2100 AD.
 
-lunarDate = ZhDate(2024, 5, 5)
-print(lunarDate)  #農曆
+def lunarDateToDate(year, lunar_month, lunar_day):
+	lunarDate = ZhDate(year, lunar_month, lunar_day)
+	print(lunarDate)  #農曆
 
-solarDate = lunarDate.to_datetime()
-print(solarDate)  #國曆
+	solarDate = lunarDate.to_datetime()
+	print(solarDate)  #國曆
+	return solarDate
+
+
+def create_google_calendar(Subject, Start_Date, All_Day_Event, Start_Time, End_Time, Location, Description):
+	return
+
+
+lunarDateToDate(2024,5,5)
