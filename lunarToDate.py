1b77d1b223903ac1e080492c532f228f2af66f48
ck.yeh <ck.yeh@axiomtek.com.tw>
2024-01-09 08:13:54 +0800

    add print

--- lunarToDate.py
+++ lunarToDate.py
@@ -1,5 +1,6 @@
-from zhdate import ZhDate
 # zhdate is limited to before 2100 AD.
+# https://github.com/CutePandaSh/zhdate/tree/master
+from zhdate import ZhDate
 
 def lunarDateToDate(year, lunar_month, lunar_day):
 	lunarDate = ZhDate(year, lunar_month, lunar_day)
@@ -10,8 +11,13 @@ def lunarDateToDate(year, lunar_month, lunar_day):
 	return solarDate
 
 
-def create_google_calendar(Subject, Start_Date, All_Day_Event, Start_Time, End_Time, Location, Description):
+def create_google_calendar(Subject, Start_Date, End_Date, isAll_Day_Event, Start_Time, End_Time, Location, Description,isPrivate):
+	print (Subject + "," + Start_Time)
 	return
 
 
-lunarDateToDate(2024,5,5)
+solarDate = lunarDateToDate(2024,5,5)
+print(solarDate.Date)
+
+
+create_google_calendar("test", "2024/5/10", "2024/5/10", False, "12:00 PM", "01:30 PM", "Taipei","No", True)
